1da14fa7aa27412755c18aacc462bcd373f914b0
Will Farrell <will@mojotech.com>
2015-03-05 22:35:01 -0500

    hawk_www: Update favicon.

--- hawk_www/src/main/clj/hawk_www/markup.clj
+++ hawk_www/src/main/clj/hawk_www/markup.clj
@@ -38,9 +38,10 @@
     [:html
      [:head
       [:title]
-      [:link {:rel "icon" :type "image/png" :href "/favicon-32x32.png" :sizes "32x32"}]
-      [:link {:rel "icon" :type "image/png" :href "/favicon-96x96.png" :sizes "96x96"}]
-      [:link {:rel "icon" :type "image/png" :href "/favicon-16x16.png" :sizes "16x16"}]]
+      [:link {:rel "shortcut icon" :type "image/x-icon" :href "favicon.ico"}]
+      [:link {:rel "icon" :type "image/png" :href "favicon-32x32.png" :sizes "32x32"}]
+      [:link {:rel "icon" :type "image/png" :href "favicon-96x96.png" :sizes "96x96"}]
+      [:link {:rel "icon" :type "image/png" :href "favicon-16x16.png" :sizes "16x16"}]]
      [:body
       [:a
        [:img.ribbon {:src "img/github-banner.png" :alt "Fork me on GitHub"}]]
