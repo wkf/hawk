e54ff5e82c393bbf93c3db65a551d76eb506b6dd
Will Farrell <will@mojotech.com>
2016-01-13 14:28:14 -0500

    Version 0.2.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hawk "0.2.6"
+(defproject hawk "0.2.7-SNAPSHOT"
   :description "Watch files with clojure (in the manner of a hawk)"
   :test-paths ["src/test"]
   :source-paths ["src/main"]
